2fbab26ef245b7dab7b050f42be8fca687ca8599
Jeffrinissac <110784221+Jeffrinissac@users.noreply.github.com>
2022-08-20 19:27:46 -0400

    Add files via upload

---
+++ J - Task 1 (12-08-22).py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 1
+y = 2
+print(x,y)
+
+
+# In[2]:
+
+
+x = (1,2)
+print(x)
+
+
+# In[13]:
+
+
+x = "happy"
+y = "morning"
+print(x,y)
+
+
+# In[23]:
+
+
+x = y = "lily","lotus","jasmine"
+print(x)
+print(y)
+
+
+# In[28]:
+
+
+samantha = ["cute", "sweet", "hardworker"]
+x,y,z = samantha
+print(x)
+print(y)
+print(z)
+
+
+# In[ ]:
+
+
+
+
---
+++ J - Task 2 (18-08-22).py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=7
+b=3
+print(a+b)
+
+
+# In[4]:
+
+
+a=14
+b=8
+print(a-b)
+
+
+# In[5]:
+
+
+a=5
+b=3
+print(a*b)
+
+
+# In[6]:
+
+
+a=18
+b=6
+print(a/b)
+
+
+# In[10]:
+
+
+a=3
+b=5
+print(a%b)
+
+
+# In[13]:
+
+
+a=5
+b=2
+print(a%b)
+
+
+# In[14]:
+
+
+a&=4
+print(a)
+
+
+# In[15]:
+
+
+a==5
+b=3
+print(a==b)
+
+
+# In[18]:
+
+
+a=2
+b|=4
+print(a|b)
+
+
+# In[19]:
+
+
+a<=7
+b<=2
+print(a<b)
+
+
+# In[7]:
+
+
+a = 5
+b = 2.0
+c = 1j+4
+print(type(a))
+print(type("a"))
+print(type(False))
+print(type(b))
+print(type(c))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
