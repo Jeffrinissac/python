b8fa8076ff3bf609a50e9cf046452b56029f2422
Jeffrinissac <110784221+Jeffrinissac@users.noreply.github.com>
2022-08-20 15:09:59 -0400

    Add files via upload

---
+++ J - Task 3 (19-08-22).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+r=int(input("radius "))
+p=3.14
+
+diameter = (2*r)
+circumference = (2*p*r)
+area = (p*r*r)
+
+#diameter
+print(diameter)
+print(int(diameter))
+print(float(diameter))
+print(type(diameter))
+
+#circumference
+print(circumference)
+print(int(circumference))
+print(float(circumference))
+print(type(circumference))
+
+#area
+print(area)
+print(int(area))
+print(float(area))
+print(type(area))
+
