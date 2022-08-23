3272a94b8f73502dd5188d0a88b312c9140dd386
Jeffrinissac <110784221+Jeffrinissac@users.noreply.github.com>
2022-08-22 23:01:44 -0400

    Add files via upload

---
+++ J - Task 5 (24-08-22)..py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[51]:
+
+
+a = "Information_Technology"
+print (a[-20:-9])
+print (a[-14:])
+print (a[-7:])
+print(a[-21:-1])
+print(a[-22:-9])
+
+
+# In[89]:
+
+
+b = "python_programming_Language"
+print(b[-25:-3])
+print(b[-20:])
+print(b[-26:-20])
+print(b[-15:-9])
+print(b[-18:-12])
+print(b[-11:-2])
+print(b[-18:])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
